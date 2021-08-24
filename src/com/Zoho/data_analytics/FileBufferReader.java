66ff00d1713657594d2b82622f9c70850b82ea54
kaarthikeyan28 <kaarthikeyan2807@gmail.com>
2021-08-25 00:34:02 +0530

    Packages Added

--- src/com/Zoho/data_analytics/FileBufferReader.java
+++
@@ -1,9 +0,0 @@
-package com.Zoho.data_analytics;
-
-import java.util.List;
-
-public interface FileBufferReader {
-    public List<String> readFiles(String filename);
-    public void convertLines(List<String> lines);
-}
-
--- src/com/Zoho/data_analytics/Main.java
+++
@@ -1,8 +0,0 @@
-package com.Zoho.data_analytics;
-
-public class Main{
-    public static void main(String[] args) {
-        UI userObj = new UI();
-        userObj.Home();
-    }
-}
